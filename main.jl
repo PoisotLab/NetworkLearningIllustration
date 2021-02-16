7179e44ac18cf37b28ceed9c598e1737f448c4c4
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-02-16 11:01:06 -0500

    first commit

---
+++ main.jl
@@ -0,0 +1,214 @@
+using Flux
+using EcologicalNetworks
+using MultivariateStats
+using StatsPlots
+using ProgressMeter
+using LinearAlgebra
+using TSne
+using Clustering
+using Loess
+
+theme(:mute)
+
+ids = map(i -> i.ID, filter(i -> contains(i.Reference, "Hadfield"), web_of_life()))
+B = convert.(BipartiteNetwork, web_of_life.(ids))
+M = reduce(∪, B)
+
+N = convert(UnipartiteNetwork, M)
+K = EcologicalNetworks.mirror(N)
+
+pc = fit(PPCA, Float64.(Array(K.edges)))
+pr = MultivariateStats.transform(pc, Float64.(Array(K.edges)))
+scatter(pr[:,1], pr[:,2], frame=:origin, alpha=0.6, lab=false, aspectratio=1, dpi=400, size=(400,400))
+xaxis!("PC1")
+yaxis!("PC2")
+savefig("features.png")
+
+nf = 14
+cooc = zeros(Bool, prod(size(M)))
+labels = zeros(Bool, prod(size(M)))
+features = zeros(Float64, (2*nf, prod(size(M))))
+cursor = 0
+for i in species(M; dims=1)
+    for j in species(M; dims=2)
+        global cursor += 1
+        # Interaction in the metaweb?
+        labels[cursor] = M[i,j]
+        # Values in the PCA space
+        p_i = findfirst(i .== species(N))
+        p_j = findfirst(j .== species(N))
+        features[1:nf, cursor] .= pr[1:nf,p_i]
+        features[(nf+1):end, cursor] .= pr[1:nf,p_j]
+        # Co-occurrence?
+        for b in B
+            if i in species(b)
+                if j in species(b)
+                    cooc[cursor] = true
+                end
+            end
+        end
+    end
+end
+
+kept = findall(cooc)
+x = Float32.(copy(features[:, kept]))
+y = Flux.onehotbatch(labels[kept], [false, true])
+
+training_size = convert(Int64, floor(size(x, 2)*0.8))
+train = sort(sample(1:size(x,2), training_size, replace=false))
+test = filter(i -> !(i in train), 1:size(x,2))
+
+data = (x[:,train], y[:,train])
+data_test = (x[:,test], y[:,test])
+
+m = Chain(
+    Dense(2nf, 3nf, relu),
+    Dropout(0.8),
+    Dense(3nf, nf, σ),
+    Dropout(0.5),
+    Dense(nf, 8, σ),
+    Dropout(0.5),
+    Dense(8, 2, σ),
+    Dropout(0.5),
+    softmax
+)
+include("plotnetwork.jl")
+savefig("network-untrained.png")
+
+function confusion_matrix(model, f, l)
+    pred = Flux.onecold(model(f), [false, true])
+    obs = Flux.onecold(l, [false, true])
+    M = zeros(Int64, (2,2))
+    M[1,1] = sum(pred .* obs)
+    M[2,2] = sum(.!pred .* .!obs)
+    M[1,2] = sum(pred .> obs)
+    M[2,1] = sum(pred .< obs)
+    return M
+end
+
+loss(x, y) = Flux.logitcrossentropy(m(x), y)
+ps = Flux.params(m)
+opt = ADAM()
+
+n_batches, batch_size = 25000, 16
+matrices_train = zeros(Int64, (2,2,n_batches))
+matrices_test = zeros(Int64, (2,2,n_batches))
+
+@showprogress for i in 1:n_batches
+    ord = sample(train, batch_size, replace=false)
+    data_batch = (x[:,ord], y[:,ord])
+    while sum(data_batch[2],dims=2)[2] < 0.3batch_size
+        ord = sample(train, batch_size, replace=false)
+        data_batch = (x[:,ord], y[:,ord])
+    end
+    Flux.train!(loss, ps, [data_batch], opt)
+    matrices_test[:,:,i] = confusion_matrix(m, data_test...)
+    matrices_train[:,:,i] = confusion_matrix(m, data...)
+end
+include("plotnetwork.jl")
+savefig("network-trained.png")
+
+accuracy = (M) -> sum(diag(M)) / sum(M)
+sensitivity = (M) -> M[1,1]/(M[1,1]+M[2,1])
+specificity = (M) -> M[2,2]/(M[1,2]+M[2,2])
+tss = (M) -> (M[1,1]*M[2,2]-M[1,2]*M[2,1])/((M[1,1]+M[2,1])*(M[1,2]+M[2,2]))
+
+plot(
+    vec(mapslices(specificity, matrices_train, dims=[1,2])),
+    lab = "Training", ylab="Specificity", c=:orange, 
+    legend=:bottomleft
+)
+plot!(
+    vec(mapslices(specificity, matrices_test, dims=[1,2])),
+    lab="Validation", c=:teal, lw=2.0
+)
+
+plot(
+    vec(mapslices(accuracy, matrices_train, dims=[1,2])),
+    lab = "Training", ylab="Accuracy",
+    legend=:bottomleft, frame=:box,
+    dpi=400, size=(400,400)
+)
+plot!(
+    vec(mapslices(accuracy, matrices_test, dims=[1,2])),
+    lab="Validation", lw=2.0
+)
+xaxis!((0, 25000), "Epoch")
+yaxis!((0,1), "Accuracy")
+savefig("validation.png")
+
+plot(
+    vec(mapslices(tss, matrices_train, dims=[1,2])),
+    lab = "Training", ylab="True-Skill Statistic", c=:orange, 
+    legend=:bottomleft
+)
+plot!(
+    vec(mapslices(tss, matrices_test, dims=[1,2])),
+    lab="Validation", c=:teal, lw=2.0
+)
+
+plot(
+    vec(mapslices(sensitivity, matrices_train, dims=[1,2])),
+    lab = "Training", ylab="Sensitivity",
+    legend=:bottomleft
+)
+plot!(
+    vec(mapslices(sensitivity, matrices_test, dims=[1,2])),
+    lab="Validation", lw=2.0
+)
+xaxis!((0, 25000), "Epoch")
+yaxis!((0,1), "Sensitivity")
+savefig("validation2.png")
+
+predictions = Flux.onecold(m(features), [false, true])
+P = copy(M) 
+P.edges[findall(predictions)] .= true
+
+eN = AJS(N)
+histogram(last.(eN))
+
+eP = AJS(P)
+histogram(last.(eP))
+
+function dk(N; kw...)
+    d = collect(values(degree(N; kw...)))
+    k = 1:1:maximum(d)
+    pk = zeros(Float64, length(k))
+    for (i,K) in enumerate(k)
+        pk[i] = sum(d .== K)/length(d)
+    end
+    kp = findall(pk .> 0)
+    return (k[kp], pk[kp])
+end
+
+plot(dpi=400, frame=:box, xlabel="k", ylabel="P(k)")
+pkn = dk(N)
+model = loess(pkn...)
+us = range(extrema(pkn[1])...; step = 0.1)
+vs = Loess.predict(model, us)
+plot!(us, vs, c=:grey, lab="")
+scatter!(pkn, lab="Observed", alpha=0.4)
+pkn = dk(P)
+model = loess(pkn...)
+us = range(extrema(pkn[1])...; step = 0.1)
+vs = Loess.predict(model, us)
+plot!(us, vs, c=:darkgrey, lab="", ls=:dash)
+scatter!(pkn, lab="Imputed", alpha=0.6, m=:diamond)
+
+
+plot(dpi=400, frame=:box, size=(400,400))
+density!(collect(values(degree(N))), fill=(0, 0.2), lab="Empirical")
+density!(collect(values(degree(P))), fill=(0, 0.2), ls=:dash, lab="Imputed")
+xaxis!((0, 120), "Degree")
+yaxis!((0, 0.06), "Density")
+savefig("degree.png")
+
+emb_pre = tsne(convert.(Float64, Array(N.edges)), nf, 2, 1000, 5, pca_init=true)
+km = kmeans(emb_pre', 6)
+scatter(emb_pre[:,1], emb_pre[:,2], frame=:none, lab="", marker_z=assignments(km), c=:Dark2, legend=false, msw=0.5, aspectratio=1, size=(500, 500), dpi=400)
+savefig("tsne-original.png")
+
+emb_post = tsne(convert.(Float64, Array(convert(UnipartiteNetwork, P).edges)), nf, 2, 1000, 5, pca_init=true)
+km = kmeans(emb_post', 2)
+scatter(emb_post[:,1], emb_post[:,2], frame=:none, lab="", marker_z=assignments(km), c=:Dark2, legend=false, msw=0.5, aspectratio=1, size=(500, 500), dpi=400)
+savefig("tsne-imputed.png")
---
+++ plotnetwork.jl
@@ -0,0 +1,50 @@
+function sc(x, m, M)
+    y = (x .- minimum(x))./(maximum(x)-minimum(x))
+    y .*= M-m 
+    y .+= m
+    return y
+end
+
+plot(frame=:none, dpi=400, size=(800,600), legend=false)
+n_input = size(params(m)[1],2) 
+lay1 = sc(collect(1:(n_input+0.5n_input-1)), -1.2, 1.2)
+y_in = lay1[vcat(collect(1:3:length(lay1)), collect(2:3:length(lay1)))]
+
+# Scaling
+n2 = size(params(m)[1],1)
+y_2 = sc(collect(1:n2), -n2/n_input, n2/n_input)
+for i in eachindex(y_in)
+    for j in eachindex(y_2)
+        plot!([1, 2], [y_in[i], y_2[j]], lab=false, line_z = params(m)[1][j,i], alpha=abs(params(m)[1][j,i])^1.2, clim=(-1,1), c=:PuOr)
+    end
+end
+
+n3 = size(params(m)[3],1)
+y_3 = sc(collect(1:n3), -n3/n_input, n3/n_input)
+for i in eachindex(y_2)
+    for j in eachindex(y_3)
+        plot!([2, 3], [y_2[i], y_3[j]], lab=false, line_z = params(m)[3][j,i], alpha=abs(params(m)[3][j,i])^1.2, clim=(-1,1), c=:PuOr)
+    end
+end
+
+n4 = size(params(m)[5],1)
+y_4 = sc(collect(1:n4), -n4/n_input, n4/n_input)
+for i in eachindex(y_3)
+    for j in eachindex(y_4)
+        plot!([3, 4], [y_3[i], y_4[j]], lab=false, line_z = params(m)[5][j,i], alpha=abs(params(m)[5][j,i])^1.2, clim=(-1,1), c=:PuOr)
+    end
+end
+
+n5 = size(params(m)[7],1)
+y_5 = sc(collect(1:n5), -n5/n_input, n5/n_input)
+for i in eachindex(y_4)
+    for j in eachindex(y_5)
+        plot!([4, 5], [y_4[i], y_5[j]], lab=false, line_z = params(m)[7][j,i], alpha=abs(params(m)[7][j,i])^1.2, clim=(-1,1), c=:PuOr)
+    end
+end
+
+scatter!(fill(1, length(y_in)), y_in, m=:circle, msw=0.0, c=:grey, lab="", ms=1.4)
+scatter!(fill(2, length(params(m)[2])), y_2, marker_z = params(m)[2], c=:PuOr, msw=0.2, lab="", clim=(-1,1))
+scatter!(fill(3, length(params(m)[4])), y_3, marker_z = params(m)[4], c=:PuOr, msw=0.2, lab="", clim=(-1,1))
+scatter!(fill(4, length(params(m)[6])), y_4, marker_z = params(m)[6], c=:PuOr, msw=0.2, lab="", clim=(-1,1))
+scatter!(fill(5, length(params(m)[8])), y_5, marker_z = params(m)[8], c=:PuOr, msw=0.2, lab="", clim=(-1,1))
